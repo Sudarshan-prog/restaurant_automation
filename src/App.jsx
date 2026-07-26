b5d1427a3a9ddb476c5742ee56433a8474cfe43b
Sudarshan-prog <epicvisionsan@gmail.com>
2026-07-26 17:10:04 +0530

    Added Dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import RestaurantDetails from './RestaurantDetails';
 import TablesSetup from './TablesSetup';
 import MenuSetup from './MenuSetup';
 import FinishSetup from './FinishSetup';
+import Dashboard from './Dashboard';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('auth');
@@ -20,7 +21,8 @@ function App() {
       {currentPage === 'details' && <RestaurantDetails onBack={() => navigateTo('setup')} onNext={() => navigateTo('tables')} />}
       {currentPage === 'tables' && <TablesSetup onBack={() => navigateTo('details')} onNext={() => navigateTo('menu')} />}
       {currentPage === 'menu' && <MenuSetup onBack={() => navigateTo('tables')} onNext={() => navigateTo('finish')} />}
-      {currentPage === 'finish' && <FinishSetup onBack={() => navigateTo('menu')} onLaunch={() => navigateTo('auth')} />}
+      {currentPage === 'finish' && <FinishSetup onBack={() => navigateTo('menu')} onLaunch={() => navigateTo('dashboard')} />}
+      {currentPage === 'dashboard' && <Dashboard />}
     </>
   );
 }
